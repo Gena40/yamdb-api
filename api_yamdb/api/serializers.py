827d595e83b85c8fd3f3bd6284146ad7b1220d34
Александр Бебякин <lorpaxx@yandex.ru>
2022-02-01 11:27:21 +0300

    add model Category, Genre, Title, Genre_Title, add serializer for them, add simple-jwt in requirements

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,11 @@
+from django.db.models import Avg
 from rest_framework import serializers
 # from rest_framework.relations import SlugRelatedField
 from reviews.models import Review, Comment
+from reviews.models import Category
+from reviews.models import Genre
+from reviews.models import Title
+# from reviews.models import Genre_Title
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -20,3 +25,43 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ('id', 'text', 'author', 'pub_date')
         # read_only_fields = ('post', 'created')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    '''
+    Класс CategorySerializer для модели Category.
+    '''
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    '''
+    Класс GenreSerializer для модели Genre.
+    '''
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    '''
+    Класс TitleSerializer для модели Title.
+    '''
+    rating = serializers.SerializerMethodField()
+    genre = GenreSerializer(many=True, required=True)
+    category = CategorySerializer(many=False, required=True)
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'rating',
+            'description', 'genre', 'category'
+        )
+
+    def get_rating(self, title_obj):
+        if title_obj.reviews.count() > 0:
+            score = title_obj.reviews.aggregate(Avg('score'))
+            return score.get('score__id')
+        return None
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -44,3 +44,122 @@ class Comment(models.Model):
             'Коментарий пользователя '
             f'{self.author} к отзыву с ID = {self.review.id}'
         )
+
+
+class Category(models.Model):
+    '''
+    Класс Category.
+    '''
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Категория',
+        help_text='Категория',
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Slug категории',
+        help_text='Slug категории'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        '''
+        При обращении к экземпляру возвращаем slug для ссылки на категорию.
+        '''
+        return f'Категория: {self.slug}'
+
+
+class Genre(models.Model):
+    '''
+    Класс Genre.
+    '''
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Жанр',
+        help_text='Жанр',
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Slug жанра',
+        help_text='Slug жанра'
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        '''
+        При обращении к экземпляру возвращаем slug для ссылки на жанр.
+        '''
+        return f'Жанр: {self.slug}'
+
+
+class Title(models.Model):
+    '''
+    Класс Title.
+    '''
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Произведение',
+        help_text='Произведение',
+    )
+    year = models.IntegerField(
+        verbose_name='Год выпуска',
+        help_text='Год выпуска'
+    )
+    description = models.TextField(
+        verbose_name='Кратное описание произведения',
+        help_text='Кратное описание произведения',
+        blank=True,
+        null=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='Категория',
+        help_text='Категория'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='Genre_Title',
+        verbose_name='Жанр',
+        help_text='Жанр',
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        '''
+        При обращении к экземпляру возвращаем name.
+        '''
+        return self.name
+
+
+class Genre_Title(models.Model):
+    '''
+    Класс Genre_Title.
+    '''
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        verbose_name='Жанр',
+        help_text='Жанр',
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение',
+        help_text='Произведение',
+    )
+
+    def __str__(self):
+        return f'Жанр {self.genre} для произведения {self.title}'
